a3d7c864a61eb78a044122dfff11b132c5d33751
Andrés Del Carpio <andresdc@andresdc.net>
2020-05-20 17:21:45 -0500

    Added validation to Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,8 @@
 class Game < ApplicationRecord
+  validates :name, :genre, :price, :release_date, presence: true
+  validates :name, uniqueness: true, length: { maximum: 40 }
+  validates :genre, inclusion: {
+    in: ['Simulator', 'Adventure', 'Strategy', 'Role-playing (RPG)', 'Shooter', 'Fighting', 'Sport'],
+  }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
